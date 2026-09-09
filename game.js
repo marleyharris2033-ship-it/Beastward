393885a707f98b8eb485ca4f58d787b4c64360f6
marleyharris2033-ship-it <marleyharris2033@gmail.com>
2026-09-09 22:26:50 +0100

    Load stars and quest progression system

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-// Beastward loader v31
+// Beastward loader v32
 (() => {
   const core=document.createElement('script');
   core.src='game-core.js?v=20260909-57';
@@ -8,6 +8,11 @@
     patch.onload=()=>{
       const synergy=document.createElement('script');
       synergy.src='synergy-patch.js?v=20260909-68';
+      synergy.onload=()=>{
+        const progression=document.createElement('script');
+        progression.src='progression-patch.js?v=20260909-69';
+        document.head.appendChild(progression);
+      };
       document.head.appendChild(synergy);
     };
     document.head.appendChild(patch);
