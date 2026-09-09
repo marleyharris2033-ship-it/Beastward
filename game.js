a99f33786d7ff945a465883a315a748cdbe90085
marleyharris2033-ship-it <marleyharris2033@gmail.com>
2026-09-09 14:58:09 +0100

    Deploy long-term beast progression rebalance

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
 // Beastward loader v22
 (() => {
   const core = document.createElement('script');
-  core.src = 'game-core.js?v=20260909-53';
+  core.src = 'game-core.js?v=20260909-54';
   core.onload = () => {
     const patch = document.createElement('script');
-    patch.src = 'range-sprite-patch.js?v=20260909-53';
+    patch.src = 'range-sprite-patch.js?v=20260909-54';
     document.head.appendChild(patch);
   };
   document.head.appendChild(core);
