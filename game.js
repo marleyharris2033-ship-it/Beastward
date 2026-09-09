bc4c3660acc66af0bf816fcb987fc5e2bebbf72e
marleyharris2033-ship-it <marleyharris2033@gmail.com>
2026-09-09 08:54:59 +0100

    Refresh drag placement build

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
 // Beastward loader v22
 (() => {
   const core = document.createElement('script');
-  core.src = 'game-core.js?v=20260909-24';
+  core.src = 'game-core.js?v=20260909-25';
   core.onload = () => {
     const patch = document.createElement('script');
-    patch.src = 'range-sprite-patch.js?v=20260909-24';
+    patch.src = 'range-sprite-patch.js?v=20260909-25';
     document.head.appendChild(patch);
   };
   document.head.appendChild(core);
