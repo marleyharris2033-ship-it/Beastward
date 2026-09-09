15516aeea668be257d967557b296726cd1c47792
marleyharris2033-ship-it <marleyharris2033@gmail.com>
2026-09-09 15:20:50 +0100

    Refresh World 2 campaign build

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
 // Beastward loader v22
 (() => {
   const core = document.createElement('script');
-  core.src = 'game-core.js?v=20260909-55';
+  core.src = 'game-core.js?v=20260909-56';
   core.onload = () => {
     const patch = document.createElement('script');
-    patch.src = 'range-sprite-patch.js?v=20260909-55';
+    patch.src = 'range-sprite-patch.js?v=20260909-56';
     document.head.appendChild(patch);
   };
   document.head.appendChild(core);
