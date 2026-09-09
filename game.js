a3e175dc230293becc36cc6a29e68238ef6854e6
marleyharris2033-ship-it <marleyharris2033@gmail.com>
2026-09-09 21:40:19 +0100

    Load team synergy system

--- game.js
+++ game.js
@@ -1,7 +1,16 @@
-// Beastward loader v29
+// Beastward loader v30
 (() => {
   const core=document.createElement('script');
   core.src='game-core.js?v=20260909-57';
-  core.onload=()=>{const patch=document.createElement('script');patch.src='range-sprite-patch.js?v=20260909-66';document.head.appendChild(patch)};
+  core.onload=()=>{
+    const patch=document.createElement('script');
+    patch.src='range-sprite-patch.js?v=20260909-66';
+    patch.onload=()=>{
+      const synergy=document.createElement('script');
+      synergy.src='synergy-patch.js?v=20260909-67';
+      document.head.appendChild(synergy);
+    };
+    document.head.appendChild(patch);
+  };
   document.head.appendChild(core);
 })();
