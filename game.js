7cdfbd3c5ad24994dad060db7d92c9b0aecbb2d9
marleyharris2033-ship-it <marleyharris2033@gmail.com>
2026-09-10 17:29:37 +0100

    Remove poor simple sprite experiment

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-// BeastBorn loader v45
+// BeastBorn loader v46
 (() => {
   const core=document.createElement('script');
   core.src='game-core.js?v=20260909-57';
@@ -26,11 +26,6 @@
                 finalEvo.onload=()=>{
                   const sprigpawArt=document.createElement('script');
                   sprigpawArt.src='sprigpaw-art-patch.js?v=20260910-84';
-                  sprigpawArt.onload=()=>{
-                    const simpleSprites=document.createElement('script');
-                    simpleSprites.src='simple-sprite-patch.js?v=20260910-1';
-                    document.head.appendChild(simpleSprites);
-                  };
                   document.head.appendChild(sprigpawArt);
                 };
                 document.head.appendChild(finalEvo);
