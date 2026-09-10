f86dd53e7a29bb7555402cc78634b624b151362b
marleyharris2033-ship-it <marleyharris2033@gmail.com>
2026-09-10 19:25:07 +0100

    Refresh fixed landscape battle mode

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-// BeastBorn loader v48
+// BeastBorn loader v49
 (() => {
   const core=document.createElement('script');
   core.src='game-core.js?v=20260909-57';
@@ -19,8 +19,8 @@
                 finalEvo.onload=()=>{
                   const sprigpawArt=document.createElement('script');sprigpawArt.src='sprigpaw-art-patch.js?v=20260910-84';
                   sprigpawArt.onload=()=>{
-                    const desert=document.createElement('script');desert.src='desert-region-patch.js?v=20260910-1';
-                    desert.onload=()=>{const landscape=document.createElement('script');landscape.src='battle-landscape-patch.js?v=20260910-1';document.head.appendChild(landscape);};
+                    const desert=document.createElement('script');desert.src='desert-region-patch.js?v=20260910-4';
+                    desert.onload=()=>{const landscape=document.createElement('script');landscape.src='battle-landscape-patch.js?v=20260910-4';document.head.appendChild(landscape);};
                     document.head.appendChild(desert);
                   };
                   document.head.appendChild(sprigpawArt);
